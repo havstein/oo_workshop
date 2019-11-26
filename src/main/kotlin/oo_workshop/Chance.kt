c01ee48b406e6d826b98f4acdc7990f66bbb94d3
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2019-11-26 12:11:09 +0100

    tester `not()`

---
+++ src/main/kotlin/oo_workshop/Chance.kt
@@ -0,0 +1,12 @@
+package oo_workshop
+
+class Chance(private val probability: Double) {
+    operator fun not(): Chance {
+        return Chance(1 - probability)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other !is Chance) return false
+        return this.probability == other.probability
+    }
+}
---
+++ src/test/kotlin/oo_workshop/ChanceTest.kt
@@ -0,0 +1,36 @@
+package oo_workshop
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+internal class ChanceTest {
+
+    @Nested
+    inner class Not {
+        @Test
+        internal fun `ikke 1 er 0`() {
+            assertEquals(Chance(0.0), !Chance(1.0))
+        }
+
+        @Test
+        internal fun `ikke 0 er 1`() {
+            assertEquals(Chance(1.0), !Chance(0.0))
+        }
+
+        @Test
+        internal fun `ikke 0,5 er 0,5`() {
+            assertEquals(Chance(0.5), !Chance(0.5))
+        }
+
+        @ParameterizedTest
+        @ValueSource(doubles = [0.0, 0.5, 1.0])
+        internal fun `dobbel-ikke`(probability: Double) {
+            Chance(probability).also {
+                assertEquals(it, !!it)
+            }
+        }
+    }
+}
